5a8d3f5d7f7fcaaa6bba758f178dcd8a53046b17
mihail petrov <mr.mihail.petrov@gmail.com>
2016-03-24 23:26:56 +0200

    first touch

---
+++ src/HomeTown.java
@@ -0,0 +1,9 @@
+/**
+ * Created by messynger on 24.03.16.
+ */
+public class HomeTown {
+    public static void main(String[] args) {
+        String city = "Sofia";
+        System.out.println(String.format("My hometown is %s ", city ));
+    }
+}
